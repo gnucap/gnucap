32217dd5bc4fc70209391021b578c51e81e78483
Felix Salfelder <felix@salfelder.org>
2015-09-11 12:06:55 +0200

    fix another memory leak in map_sckt_nodes
    delete map in case of an exception.

--- lib/e_cardlist.cc
+++ lib/e_cardlist.cc
@@ -503,7 +503,12 @@ void CARD_LIST::map_subckt_nodes(const CARD* model, const CARD* owner)
     if ((**ci).is_device()) {
       for (int ii = 0;  ii < (**ci).net_nodes();  ++ii) {
 	// for each connection node in card
-	(**ci).n_(ii).map_subckt_node(map, owner);
+	try{ untested();
+	  (**ci).n_(ii).map_subckt_node(map, owner);
+	}catch(...){ untested();
+	  delete[] map;
+	  throw;
+	}
       }
     }else{
       assert(dynamic_cast<MODEL_CARD*>(*ci));
