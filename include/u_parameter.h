639aca36e39f345f192c9b18d0c47b0a1f4f0fc1
Felix Salfelder <felix@salfelder.org>
2018-06-29 11:33:03 +0100

    const access to parameters

--- include/u_parameter.h
+++ include/u_parameter.h
@@ -222,6 +222,8 @@ public:
 
   iterator begin() {return _pl.begin();}
   iterator end() {return _pl.end();}
+  const_iterator begin()const {untested(); return _pl.begin();}
+  const_iterator end()const {untested(); return _pl.end();}
 private:
   mutable int _index;
   mutable const_iterator _previous;
