e6e774b37529568460c1cd65a5ab9af939b411ba
Felix Salfelder <felix@salfelder.org>
2022-11-02 00:00:00 +0000

    CARD::deflate

--- include/e_card.h
+++ include/e_card.h
@@ -68,6 +68,7 @@ public:	// "elaborate"
   virtual void	 expand_last()		{}
   virtual void	 precalc_last()		{}
   virtual void	 map_nodes()		{}
+  virtual CARD*  deflate()		{untested(); return this;}
   //--------------------------------------------------------------------
 public:	// dc-tran
   virtual void	 tr_iwant_matrix()	{}
