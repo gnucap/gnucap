e7efda564c2a0c84a05452c675d0c1feb492b7c0
Felix Salfelder <felix@salfelder.org>
2024-05-04 00:00:00 +0000

    io_trace.h: C code compliance
    avoid iostream include unless __cplusplus.

--- include/io_trace.h
+++ include/io_trace.h
@@ -23,7 +23,9 @@
  * trace macros for model debugging
  */
 //testing=trivial 2006.07.17
+#ifdef __cplusplus
 #include <iostream>
+#endif
 
 /* allow multiple inclusions with different DO_TRACE */
 #undef trace_line
@@ -107,20 +109,20 @@
 
 #else // no __cplusplus
 
-#define trace_line() (printf("@@#\n@#@:%s:%u:%s\n", \
-			   __FILE__, __LINE__, __func__))
-#define trace0(s) (printf("@#@%s\n", s))
-#define trace1(s,x) (printf("@#@%s  %s=%g\n", s, #x, (double)(x)))
-#define trace2(s,x,y) (printf("@#@%s  %s=%g  %s=%g\n",\
-	s, #x, (double)(x), #y, (double)(y)))
-#define trace3(s,x,y,z) (printf("@#@%s  %s=%g  %s=%g  %s=%g\n",\
-	s, #x, (double)(x), #y, (double)(y), #z, (double)(z)))
-#define trace4(s,w,x,y,z)(printf("@#@%s  %s=%g  %s=%g  %s=%g  %s=%g\n",\
-	s, #w, (double)(w), #x, (double)(x), #y, (double)(y), #z, (double)(z)))
+#define trace_line() fprintf(stderr, "@@#\n@#@:%s:%u:%s\n", \
+			   __FILE__, __LINE__, __func__)
+#define trace0(s) fprintf(stderr, "@#@%s\n", s)
+#define trace1(s,x) fprintf(stderr, "@#@%s  %s=%g\n", s, #x, (double)(x))
+#define trace2(s,x,y) fprintf(stderr, "@#@%s  %s=%g  %s=%g\n",\
+	s, #x, (double)(x), #y, (double)(y))
+#define trace3(s,x,y,z) fprintf(stderr, "@#@%s  %s=%g  %s=%g  %s=%g\n",\
+	s, #x, (double)(x), #y, (double)(y), #z, (double)(z))
+#define trace4(s,w,x,y,z)fprintf(stderr, "@#@%s  %s=%g  %s=%g  %s=%g  %s=%g\n",\
+	s, #w, (double)(w), #x, (double)(x), #y, (double)(y), #z, (double)(z))
 #define trace5(s,v,w,x,y,z)\
-	(printf("@#@%s  %s=%g  %s=%g  %s=%g  %s=%g  %s=%g\n",\
+	fprintf(stderr, "@#@%s  %s=%g  %s=%g  %s=%g  %s=%g  %s=%g\n",\
 	s, #v, (double)(v), #w, (double)(w), #x, (double)(x),\
-	#y, (double)(y), #z, (double)(z)))
+	#y, (double)(y), #z, (double)(z))
 
 #endif // __cplusplus
 
