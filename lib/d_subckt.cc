dc6bdbbd367c4bdc10b9a649b2c5d2da73670ae4
Felix Salfelder <felix@salfelder.org>
2025-02-06 00:00:00 +0000

    DEV_MODULE_PROTO
    need a second one. during build proto, verilog needs
    max_nodes==net_nodes+1, spice expects hard max.
    
    and tidy up is_device

--- lib/d_subckt.cc
+++ lib/d_subckt.cc
@@ -69,6 +69,7 @@ class DEV_SUBCKT : public BASE_SUBCKT {
   int _node_capacity{0};
 private:
   friend class DEV_SUBCKT_PROTO;
+  friend class DEV_MODULE_PROTO;
   const BASE_SUBCKT* _parent;
   std::vector<std::string> _port_name; // common?
 protected:
@@ -83,7 +84,7 @@ private:
   int		set_port_by_name(std::string&, std::string&) override;
   int set_param_by_name(std::string Name, std::string Value)override;
 private: // override virtual
-  bool		is_device()const override;
+  bool		is_device()const override	{return true;}
   char		id_letter()const override	{return 'X';}
   bool		print_type_in_spice()const override {return true;}
   std::string   value_name()const override	{return "#";}
@@ -100,40 +101,6 @@ private: // override virtual
 
   void		expand() override;
 
-private: // no ops for prototype
-	 // temporary. pending lang_verilog adjustments
-  void map_nodes()override	{if(is_device()){ BASE_SUBCKT::map_nodes();}else{} }
-  void tr_begin()override	{if(is_device()){ BASE_SUBCKT::tr_begin();}else{} }
-  void tr_restore()override	{if(is_device()){ BASE_SUBCKT::tr_restore();}else{} }
-  void ac_begin()override	{if(is_device()){ BASE_SUBCKT::ac_begin();}else{} }
-  void tr_load()override	{if(is_device()){ BASE_SUBCKT::tr_load();}else{} }
-  void tr_accept()override	{if(is_device()){ BASE_SUBCKT::tr_accept();}else{} }
-  void tr_regress()override	{if(is_device()){ BASE_SUBCKT::tr_regress();}else{} }
-  void dc_final()override	{if(is_device()){ BASE_SUBCKT::dc_final();}else{}}
-  void tr_final()override	{if(is_device()){ BASE_SUBCKT::tr_final();}else{}}
-  void tr_advance()override	{if(is_device()){ BASE_SUBCKT::tr_advance();}else{} }
-  TIME_PAIR tr_review()override	{if(is_device()){ return BASE_SUBCKT::tr_review();}else{
-                                                  return TIME_PAIR(NEVER, NEVER);}}
-  void dc_advance()override	{if(is_device()){ BASE_SUBCKT::dc_advance();}else{} }
-  void do_ac()override		{if(is_device()){ BASE_SUBCKT::do_ac();}else{} }
-  void ac_load()override	{if(is_device()){ BASE_SUBCKT::ac_load();}else{} }
-  void ac_final()override	{if(is_device()){ BASE_SUBCKT::ac_final();}else{}}
-
-  void tr_queue_eval()override{
-    if(is_device()){
-      BASE_SUBCKT::tr_queue_eval();
-    }else{
-    }
-  }
-  bool do_tr() override		{if(is_device()){ return BASE_SUBCKT::do_tr();}else{ return true;} }
-
-  bool tr_needs_eval()const override{ untested();
-    if(is_device()){untested();
-      return BASE_SUBCKT::tr_needs_eval();
-    }else{untested();
-      return false;
-    }
-  }
 private:
   void		precalc_last()override;
   double	tr_probe_num(const std::string&)const override;
@@ -156,7 +123,51 @@ public:
   static int	_count;
 } p1(&Default_SUBCKT);
 int DEV_SUBCKT::_count = -1;
-DISPATCHER<CARD>::INSTALL d0(&device_dispatcher, "module", &p1);
+/*--------------------------------------------------------------------------*/
+class DEV_MODULE_PROTO : public DEV_SUBCKT {
+private:
+  explicit	DEV_MODULE_PROTO(const DEV_MODULE_PROTO&p) : DEV_SUBCKT(p)
+  {
+    new_subckt();
+  }
+public:
+  explicit	DEV_MODULE_PROTO(COMMON_COMPONENT* c=nullptr) : DEV_SUBCKT(c) {}
+		~DEV_MODULE_PROTO(){}
+  bool		is_device()const override	{ return false;}
+  bool		makes_own_scope()const override	{ return true;}
+  CARD*		clone()const override		{ return new DEV_MODULE_PROTO(*this);}
+  CARD*		clone_instance()const override {
+    auto m = DEV_SUBCKT::clone();
+    auto s = prechecked_cast<DEV_SUBCKT*>(m);
+    s->_net_nodes = 0; // needed in v_instance: 274?? v_paramset.module.1.gc
+    s->_parent = this;
+    assert(s->is_device());
+    return m;
+  }
+private: // no-ops for prototype
+  void precalc_first()override {}
+  void expand()override {}
+  void precalc_last()override {}
+  void map_nodes()override {}
+  void tr_begin()override {}
+  void tr_load()override {}
+  TIME_PAIR tr_review() override { return TIME_PAIR(NEVER, NEVER);}
+  void tr_accept()override {}
+  void tr_advance()override {}
+  void tr_restore()override {untested();}
+  void tr_regress()override {}
+  void dc_final()override {}
+  void tr_final()override {}
+  void dc_advance()override {}
+  void ac_begin()override {}
+  void do_ac()override {}
+  void ac_load()override {}
+  void ac_final()override {}
+  bool do_tr()override { return true;}
+  bool tr_needs_eval()const override {untested(); return false;}
+  void tr_queue_eval()override {}
+} p0(&Default_SUBCKT);
+DISPATCHER<CARD>::INSTALL d0(&device_dispatcher, "module", &p0);
 /*--------------------------------------------------------------------------*/
 //BUG// fixed limit on number of ports, spice only
 #define PORTS_PER_SUBCKT 100
@@ -255,41 +266,12 @@ CARD* DEV_SUBCKT_PROTO::clone_instance()const
   }
   new_instance->_net_nodes = 0;
 #endif
-  // assert(!new_instance->subckt());
 
-  if (this == &pp){
-    // cloning from static, empty model
-    // look out for _parent in expand
-    assert(!new_instance->_parent);
-    assert(!new_instance->is_device());
-    new_instance->_parent = this;
-  }else{
-    new_instance->_parent = this;
-    //assert(common() == new_instance->common());
-
-    auto c = prechecked_cast<COMMON_PARAMLIST const*>(common());
-    for(auto p : c->_params){ untested();
-      trace2("clone instance from spice", p.first, p.second.string());
-    }
-
-  }
-
- // assert(new_instance->is_device());
+  new_instance->_parent = this;
+  assert(new_instance->is_device());
   return new_instance;
 }
 /*--------------------------------------------------------------------------*/
-/*--------------------------------------------------------------------------*/
-bool DEV_SUBCKT::is_device() const
-{
-  if(!_parent) {
-    return false;
-  }else if(_parent==&pp){
-    // spice proto
-    return true;
-  }else{
-    return true;
-  }
-}
 /*--------------------------------------------------------------------------*/
 void DEV_SUBCKT::set_port_by_index(int Index, std::string& Value)
 {
@@ -331,9 +313,7 @@ int DEV_SUBCKT::max_nodes() const
 /*--------------------------------------------------------------------------*/
 CARD_LIST* DEV_SUBCKT::scope()
 {
-  if(_parent == &pp && 0) { untested();
-    return subckt();
-  }else if(is_device()){
+  if(is_device()){
     return COMPONENT::scope();
   }else{
     return subckt();
@@ -382,7 +362,7 @@ CARD* DEV_SUBCKT::clone()const
     assert(new_instance->is_device());
   }else{
     // verilog. build proto
-    assert(!new_instance->is_device());
+    // assert(!new_instance->is_device());
     if(new_instance->subckt()){ untested();
     }else{
      new_instance->new_subckt();
@@ -494,8 +474,8 @@ void DEV_SUBCKT::expand()
   BASE_SUBCKT::expand();
   trace3("DEV_SUBCKT::expand", long_label(), max_nodes(), is_device());
 
-  if(!is_device()){
-  }else if(_parent == &pp){
+  assert(is_device());
+  if(_parent == &pp){
     COMMON_PARAMLIST const* c = prechecked_cast<COMMON_PARAMLIST const*>(common());
     assert(c);
     // first time spice
@@ -607,7 +587,8 @@ void DEV_SUBCKT::precalc_last()
     subckt()->precalc_last();
 
     assert(!is_constant()); /* because I have more work to do */
-  }else{
+  }else{ untested();
+    unreachable();
     CARD::precalc_last();
     // its a proto, bypass common clash hotfix
   }
