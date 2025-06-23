gnucap/gnucap

commit 9b3b509d5a10d61099b708ce48b65d9f53e973bd

cleanup COMMON_COMPONENT
- remove is_trivial
  use deflate instead.

- stash common temp params in BM_ACTION_BASE
  they could become system params, but not ready for it

- deal with COMMON_COMPONENT::_value
  - move to BM_BASE
  - adjust s_dc: explicit _value
  - fix res warning noise introduced earlier
  - adjust a few obsolete callbacks
  - adjust test hooks in bm_*.cc, op==

- downgrade ELEMENT::_value to double
  - some modelgen commons do not have _value
  - tweak ::set_parameters
  - todo: drop ELEMENT::_value, always use common()

- test fuzz
  - $mfactor again reachable at index 1
  - fewer dup warnings, due to deflate

apps/s_dc.cc
@@ -31,6 +31,7 @@
 namespace {
 /*--------------------------------------------------------------------------*/
 class SWEEPVAL : public COMMON_COMPONENT {
+  double _value;
 public:
   explicit	SWEEPVAL(int c=0)
     :COMMON_COMPONENT(c) {}
@@ -40,6 +41,12 @@ public:
   COMMON_COMPONENT* clone()const override{
     return new SWEEPVAL(*this);
   }
+
+public:
+  void set_value(double v) { _value = v; }
+  bool has_value()const override{ return true; }
+  double value()const override { return _value; }
+
 
 private:
   std::string name()const override{untested(); return "sweepval";}
@@ -84,7 +91,7 @@ private:
   void	final()override		{_scope->dc_final();}
   void	finish()override;
 
-  explicit DCOP(const DCOP&): SIM() {unreachable(); incomplete();}
+  explicit DCOP(const DCOP&): SIM() { untested();unreachable(); incomplete();}
 protected:
   void set_sweepval(int i, double d){
     ::status.set_up.start();
@@ -143,7 +150,7 @@ public:
   void	do_it(CS&, CARD_LIST*)override;
 private:
   void	setup(CS&)override;
-  explicit DC(const DC&): DCOP() {unreachable(); incomplete();}
+  explicit DC(const DC&): DCOP() { untested();unreachable(); incomplete();}
 };
 /*--------------------------------------------------------------------------*/
 class OP : public DCOP {
@@ -153,7 +160,7 @@ public:
   void	do_it(CS&, CARD_LIST*)override;
 private:
   void	setup(CS&)override;
-  explicit OP(const OP&): DCOP() {unreachable(); incomplete();}
+  explicit OP(const OP&): DCOP() { untested();unreachable(); incomplete();}
 };
 /*--------------------------------------------------------------------------*/
 /*--------------------------------------------------------------------------*/
@@ -293,7 +300,7 @@ void DC::setup(CS& Cmd)
 	if (ELEMENT* c = dynamic_cast<ELEMENT*>(*ci)) {
 	  _zap[_n_sweeps] = c;
 	  _param_name[_n_sweeps] = ""; // not used.
-	  trace2("_zap", c->value(), c->value().string());
+	 // trace2("_zap", c->value(), c->value().string());
 	}else{untested();
 	  throw Exception("dc/op: can't sweep " + (**ci).long_label() + '\n');
 	}

include/patchlev.h
@@ -1,1 +1,1 @@
-#define PATCHLEVEL "hsp 2025.04.29"
+#define PATCHLEVEL "cleanup_cc 2025.04.26"

apps/bm_complex.cc
@@ -66,8 +66,8 @@ bool EVAL_BM_COMPLEX::operator==(const COMMON_COMPONENT& x)const
   bool rv = p
     && _value == p->_value
     && EVAL_BM_ACTION_BASE::operator==(x);
-  if (rv) { untested();
-    untested();
+  if (rv) {
+  }else{
   }
   return rv;
 }

apps/bm_exp.cc
@@ -104,8 +104,8 @@ bool EVAL_BM_EXP::operator==(const COMMON_COMPONENT& x)const
     && _tau2== p->_tau2
     && _period== p->_period
     && EVAL_BM_ACTION_BASE::operator==(x);
-  if (rv) { untested();
-    untested();
+  if (rv) {
+  }else{
   }
   return rv;
 }

apps/bm_fit.cc
@@ -103,8 +103,8 @@ bool EVAL_BM_FIT::operator==(const COMMON_COMPONENT& x)const
     && _smooth == p->_smooth
     && _table == p->_table
     && EVAL_BM_ACTION_BASE::operator==(x);
-  if (rv) { untested();
-    untested();
+  if (rv) {
+  }else{
   }
   return rv;
 }

apps/bm_generator.cc
@@ -66,7 +66,7 @@ bool EVAL_BM_GENERATOR::operator==(const COMMON_COMPONENT& x)const
   }else{untested();
   }
   bool rv = p && EVAL_BM_ACTION_BASE::operator==(x);
-  if (rv) {untested();
+  if (rv) {
   }else{
   }
   return rv;

apps/bm_model.cc
@@ -43,13 +43,27 @@ private: // override virtual
   void		parse_common_obsolete_callback(CS&)override;
   void		print_common_obsolete_callback(OMSTREAM&, LANGUAGE*)const override;
   void		expand(const COMPONENT*)override;
-  COMMON_COMPONENT* deflate()override		{return (_func) ? _func->deflate() : this;}
-  void		tr_eval(ELEMENT*d)const override{assert(_func); _func->tr_eval(d);}
-  void		ac_eval(ELEMENT*d)const override{assert(_func); _func->ac_eval(d);}
+  COMMON_COMPONENT* deflate()override;
+  void		tr_eval(ELEMENT*d)const override{untested(); assert(_func); _func->tr_eval(d);}
+  void		ac_eval(ELEMENT*d)const override{untested(); assert(_func); _func->ac_eval(d);}
   std::string	name()const override		{itested();return modelname();}
   bool		ac_too()const override		{return true;}
+  bool has_ac_eval()const override	{untested(); return _func && _func->has_ac_eval();}
+  bool has_tr_eval()const override	{untested(); return _func && _func->has_tr_eval();}
 };
 /*--------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
+COMMON_COMPONENT* EVAL_BM_MODEL::deflate()
+{
+  trace4("EVAL_BM_MODEL::deflate0", modelname(), name(), _arglist, _func);
+  if (_func) {
+    return _func->deflate();
+  }else if(has_ext_args()) { untested();
+    return this;
+  }else{
+    return this;
+  }
+}
 /*--------------------------------------------------------------------------*/
 EVAL_BM_MODEL::EVAL_BM_MODEL(int c)
   :EVAL_BM_ACTION_BASE(c),
@@ -67,14 +81,13 @@ EVAL_BM_MODEL::EVAL_BM_MODEL(const EVAL_BM_MODEL& p)
 }
 /*--------------------------------------------------------------------------*/
 bool EVAL_BM_MODEL::operator==(const COMMON_COMPONENT& x)const
-{ untested();
+{
   const EVAL_BM_MODEL* p = dynamic_cast<const EVAL_BM_MODEL*>(&x);
   bool rv = p
     && _arglist == p->_arglist
     && EVAL_BM_ACTION_BASE::operator==(x);
-  if (rv) { untested();
-    incomplete();
-    untested();
+  if (rv) {
+  }else{ untested();
   }
   return rv;
 }
@@ -129,6 +142,7 @@ void EVAL_BM_MODEL::expand(const COMPONENT* d)
   // link to the real one
   // later, a "deflate" will push it down to proper place.
 
+  trace1("EVAL_BM_MODEL::expand", modelname());
   c->set_modelname(modelname());
   CS args(CS::_STRING, _arglist);
   c->parse_common_obsolete_callback(args);

apps/bm_poly.cc
@@ -84,9 +84,8 @@ bool EVAL_BM_POLY::operator==(const COMMON_COMPONENT& x)const
     && _abs == p->_abs
     && _c == p->_c
     && EVAL_BM_ACTION_BASE::operator==(x);
-  if (rv) { untested();
-    incomplete();
-    untested();
+  if (rv) {
+  }else{
   }
   return rv;
 }

commit ead292c968609481a9e4b4d9b30814be5e051593

fix SWEEPVAL copy constructor
bug introduced in 9b3b509d5

apps/s_dc.cc
@@ -37,7 +37,7 @@ public:
   explicit	SWEEPVAL(int c=0)
     :COMMON_COMPONENT(c) {}
   explicit	SWEEPVAL(const SWEEPVAL& p)
-    :COMMON_COMPONENT(p) {}
+    :COMMON_COMPONENT(p), _value(p._value) {}
 		~SWEEPVAL() { trace1("~SWEEPVAL", this);}
   COMMON_COMPONENT* clone()const override{
     return new SWEEPVAL(*this);

include/patchlev.h
@@ -1,1 +1,1 @@
-#define PATCHLEVEL "umatch 2025.06.04"
+#define PATCHLEVEL "sweepval 2025.06.21"