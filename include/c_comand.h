6218ab447cfb1f4e09e75e08d63aca86d96ad36b
Felix Salfelder <felix@salfelder.org>
2026-03-09 00:00:00 +0000

    CMD is a DEV_DOT

--- include/c_comand.h
+++ include/c_comand.h
@@ -29,14 +29,14 @@
 /*--------------------------------------------------------------------------*/
 class CS;
 /*--------------------------------------------------------------------------*/
-class INTERFACE CMD : public CARD {
+class INTERFACE CMD : public DEV_DOT {
 public:
-  std::string value_name()const {return "";}
+  std::string value_name()const override {return "";}
   virtual void do_it(CS&, CARD_LIST*) = 0;
   virtual ~CMD() {}
   static  void  cmdproc(CS&, CARD_LIST*);
   static  void	command(const std::string&, CARD_LIST*);
-  CARD*   clone()const override {untested(); return new DEV_DOT;} // //BUG// memory leak ?
+//  CARD*   clone()const override {untested(); return new DEV_DOT;}
 };
 /*--------------------------------------------------------------------------*/
 /*--------------------------------------------------------------------------*/
