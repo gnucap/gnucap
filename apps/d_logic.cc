053ebb9b965aa046b203a044d95d706c75ffe41e
Felix Salfelder <felix@salfelder.org>
2026-03-18 00:00:00 +0000

    fold node_l into e_logicnode.h
    reduce number of copies. the ground_node klidge will stay until we have
    something better.

--- apps/d_logic.cc
+++ apps/d_logic.cc
@@ -35,28 +35,6 @@
 #include "e_hsparam.h"
 #include "e_logicnode.h"
 /*--------------------------------------------------------------------------*/
-extern NODE ground_node;
-/*--------------------------------------------------------------------------*/
-struct node_l : node_t
-{
-  LOGIC_NODE*       operator->()      { return &data(); }
-  LOGIC_NODE const* operator->()const { return &data(); }
-private:
-  LOGIC_NODE& data() const {
-    if(n_() == &ground_node){
-      // tmp hack.
-      // think of it as an autoinserted connect module
-      // mimics traditional behaviour but probably wrong.
-      static LOGIC_NODE lg(0);
-      return lg;
-    }else{
-      auto n = prechecked_cast<LOGIC_NODE const*>(n_());
-      assert(n);
-      return *const_cast<LOGIC_NODE*>(n);
-    }
-  }
-};
-/*--------------------------------------------------------------------------*/
 namespace {
 /*--------------------------------------------------------------------------*/
 class DEV_LOGIC : public ELEMENT {
