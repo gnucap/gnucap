7829fea2a7dc355aaf4127495e0b773e2342cfbf
Felix Salfelder <felix@salfelder.org>
2026-04-22 00:00:00 +0000

    do_it: run commands from lang_verilog
    opt-in by implementing CMD::clone.

--- apps/lang_verilog.cc
+++ apps/lang_verilog.cc
@@ -429,10 +429,13 @@ DEV_DOT* LANG_VERILOG::parse_command(CS& cmd, DEV_DOT* x)
     // "module" etc gets here.
   }
   parse_attributes(cmd, x->id_tag());
-  CMD::cmdproc(cmd, scope);
-  if(dynamic_cast<CMD const*>(x)){ untested();
+  if(auto cc=dynamic_cast<CMD*>(x)){ untested();
+    std::string s;
+    cmd >> s;
+    cc->do_it(cmd, scope);
     return x;
   }else{
+    CMD::cmdproc(cmd, scope);
     x->purge();
     delete x;
     return nullptr;
