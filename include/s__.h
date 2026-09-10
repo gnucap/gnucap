f5cad0307eb9361f86f6cd6bf8c5989a429e78ac
Felix Salfelder <felix@salfelder.org>
2026-09-10 00:00:00 +0000

    persistent fourier

--- include/s__.h
+++ include/s__.h
@@ -67,7 +67,7 @@ private:
   virtual bool	is_step_rejected()const {return false;}
 
 protected:
-  explicit SIM(const SIM&s):CMD(s),_scope(nullptr) { incomplete();}
+  explicit SIM(const SIM&s): CMD(s), _scope(nullptr) {}
   explicit SIM(): CMD(),_scope(nullptr) {}
 public:
   ~SIM();
